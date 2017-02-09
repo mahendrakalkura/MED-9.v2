b5fefc1e3cb18801855385b892bb6efb83afcf65
Mahendra Kalkura <mahendra@kalkura.com>
2017-02-09 14:00:50 +0530

    source_2_all.go

--- source_2_all.go
+++ source_2_all.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/getsentry/raven-go"
+	"github.com/jmoiron/sqlx"
 	"gopkg.in/cheggaaa/pb.v1"
 	"os"
 	"os/signal"
@@ -15,11 +16,13 @@ func source_2_all(settings *Settings) {
 	signal_channel := make(chan os.Signal)
 	records_channel := make(chan Record, settings.Others.Consumers*2)
 
+	database := get_database(settings)
+
 	for index := 1; index <= settings.Others.Consumers; index++ {
-		go source_2_all_consumer(settings, records_channel)
+		go source_2_all_consumer(settings, database, records_channel)
 	}
 
-	go source_2_all_producer(settings, records_channel)
+	go source_2_all_producer(database, records_channel)
 
 	signal.Notify(signal_channel, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM)
 
@@ -28,20 +31,18 @@ func source_2_all(settings *Settings) {
 	close(records_channel)
 }
 
-func source_2_all_consumer(settings *Settings, records_channel chan Record) {
+func source_2_all_consumer(settings *Settings, database *sqlx.DB, records_channel chan Record) {
 	for record := range records_channel {
 		source_2, err := get_source_2(settings, record.Street, record.Number, record.Zip, record.City)
 		if err != nil {
 			raven.CaptureErrorAndWait(err, nil)
 		} else {
-			database := get_database(settings)
 			source_2_update(database, record, source_2)
 		}
 	}
 }
 
-func source_2_all_producer(settings *Settings, records_channel chan Record) {
-	database := get_database(settings)
+func source_2_all_producer(database *sqlx.DB, records_channel chan Record) {
 	total, rows := source_2_select_all(database)
 	progress_bar := pb.StartNew(total)
 	for rows.Next() {
